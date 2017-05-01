c57eec4ceea03b48f62956157cac4556a21f53b2
Xiaoyang Zhao <xzhao31@hawk.iit.edu>
2017-04-30 22:12:51 -0500

    Add chapter 5 and modified the appendix. Upload new figure.

--- codes/FigureIn2_3_1.m
+++ codes/FigureIn2_3_1.m
@@ -2,7 +2,8 @@
 %%{
 %% QE_European call option
 % %InitializeWorkspaceDisplay %initialize the workspace and the display parameters
-T=2;
+clearvars
+T=2;%2;
 delta_t=0.2;
 t0 = delta_t;
 inp.timeDim.timeVector = t0:delta_t:T; 
@@ -18,7 +19,7 @@ inp.assetParam.Vlong = 0.09;
 inp.assetParam.kappa = 1;
 %inp.assetParam.nu = 0.5;
 inp.assetParam.rho = -0.3;
-inp.assetParam.pathType = 'QE_m';
+inp.assetParam.pathType = 'QE';
 inp.payoffParam.putCallType = {'call'};
 %inp.priceParam.cubMethod = 'Sobol';
 inp.priceParam.cubMethod = 'IID_MC';
@@ -32,10 +33,10 @@ inp.payoffParam.strike =Strike;
 inp.priceParam.absTol = 0; %absolute tolerance
 inp.priceParam.relTol = 0.01; %one penny on the dollar relative tolerance
 
-Ntime = T/0.25; 
+Ntime = T/0.2; 
 NSim = 1e6;
-
-nu = [0,10^(-6),10^(-5),10^(-4),10^(-3),10^(-2),0.05,10^(-1),0.15,0.2];
+nu = [0,10^(-6),10^(-5),10^(-4),10^(-3),10^(-2),0.05,10^(-1),0.2];
+%nu = [0,10^(-6),10^(-5),10^(-4),10^(-3),10^(-2),0.05,10^(-1),0.5];
 ourQEprice = zeros(size(nu));
 QEprice = zeros(size(nu));
 for i = 1:length(nu)
@@ -49,6 +50,7 @@ for i = 1:length(nu)
     PP = mean(PT);
     QEprice(i) = PP*exp(-inp.assetParam.interest*T);
 end
+dif = abs(ourQEprice-QEprice);
 v0 = ourQEprice(1)*ones(size(nu));
 QEzero = zeros(size(nu));
 %}
@@ -56,40 +58,43 @@ QEzero = zeros(size(nu));
 %nu = log(nu);
 %nu = exp(nu);
 %QEprice = log(QEprice);
-n=10;
+%%{
+n=9;
 figure
-semilogx(nu(1:n),ourQEprice(1:n),'-o',nu(1:n),QEprice(1:n),'-*','LineWidth',1.25)
-axis([0 1 -1.9 16])
+semilogx(nu(1:n),QEprice(1:n),'-o',nu(1:n),ourQEprice(1:n),'-*','LineWidth',1.5)
+axis([0 1 -1.5 16])
 title('QE scheme for small \nu')
 %xlabel('log(nu)')
 xlabel('\nu')
 ylabel('Option price')
 hold on
-plot(nu(1:n),v0,'-.','LineWidth',1.25)
-hold on
-plot(nu(1:n),QEzero,'--g','LineWidth',2)
+plot(nu(1:n),v0,'--g','LineWidth',1)
+% hold on
+% plot(nu(1:n),QEzero,'--g','LineWidth',2)
 hold off
-return
+legend('QE','Modified QE', 'B-S')
+legend('location','southeast')
+%}
 %% Generate LaTex code for a table of "nu is close or equal to zero"
 clear input;
-temp = NaN(1,9);
+temp = NaN(1,8);
 call_exact = [ourGBMCallPrice.exactPrice,temp];%,SobolCallPrice_exact,temp];
 %put_exact = [GBMPutPrice_exact,temp];%,SobolPutPrice_exact,temp];
-content = [call_exact; QEprice; ourQEprice];
+content = [call_exact; QEprice; ourQEprice;dif];
 % content = [call_exact;iid_temp(1:2,:),Sobol_temp(1:2,:); ...
 %     put_exact;iid_temp(3:4,:),Sobol_temp(3:4,:)];
 input.data = content;
 % Set column labels (use empty string for no label):
-input.tableColLabels = {'0','10e-6','10e-5','10e-4','10e-3','10e-2','0.05','10e-1','0.15','0.2'};
+input.tableColLabels = {'0','1e-6','1e-5','1e-4','1e-3','1e-2','0.05','0.1','0.5'};
 % Set row labels (use empty string for no label):
-input.tableRowLabels = {'Exact price','QE','Modified QE'};
+input.tableRowLabels = {'Exact price','QE','Modified QE','Diff'};
 
 % Switch transposing/pivoting your table:
 input.transposeTable = 0;
 
 % Determine whether input.dataFormat is applied column or row based:
 input.dataFormatMode = 'column'; % use 'column' or 'row'. if not set 'colum' is used
-input.dataFormat={'%.3f',10};%,'%.4f',3,'%5.2fs',3,'%3.2E',3};
+input.dataFormat={'%.3f',9};%,'%.4f',3,'%5.2fs',3,'%3.2E',3};
 input.dataNanString = '-';
 input.tableColumnAlignment = 'c';
 % Switch table borders on/off (borders are enabled by default):
